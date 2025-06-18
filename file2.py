c60ff3f251787d18b7b1bfd95e87664ac9bd88e4
shahbaz123-gif <sajjadahmadjan30@gmail.com>
2025-06-18 21:53:14 +0500

    Add files via upload

---
+++ file2.py
@@ -0,0 +1,12 @@
+#taking the mark of the student raj
+print("enter your marks")
+math=int(input("maths:"))
+print("enter your marks")
+hindi=int(input("hindi:"))
+print("enter your marks")
+english=int(input("english:"))
+print("enter your marks")
+science=int(input("sceince:"))
+sum=math+hindi+science+english
+p=(sum/400)*100
+print("percentage",p)
